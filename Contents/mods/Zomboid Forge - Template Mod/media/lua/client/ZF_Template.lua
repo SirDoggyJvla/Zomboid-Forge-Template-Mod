dba14033235963f65a5a8904fa8e602e8271fa59
SirDoggyJvla <106160594+SirDoggyJvla@users.noreply.github.com>
2024-05-20 19:36:45 +0200

    Created mod

---
+++ Contents/mods/Zomboid Forge - Template Mod/media/lua/client/ZF_Template.lua
@@ -0,0 +1,56 @@
+--[[ ================================================ ]]--
+--[[  /~~\'      |~~\                  ~~|~    |      ]]--
+--[[  '--.||/~\  |   |/~\/~~|/~~|\  /    | \  /|/~~|  ]]--
+--[[  \__/||     |__/ \_/\__|\__| \/   \_|  \/ |\__|  ]]--
+--[[                     \__|\__|_/                   ]]--
+--[[ ================================================ ]]--
+--[[
+
+This file defines the core of the mod of The Last of Us Infected Fork
+
+]]--
+--[[ ================================================ ]]--
+
+--- import module from ZomboidForge
+local ZomboidForge = require "ZomboidForge_module"
+
+--- Create zombie types
+local function Initialize_ZF_Template()
+    -- Strong Zomboid
+	if SandboxVars.ZF_Template.StrongZomboid then
+		ZomboidForge.ZTypes.StrongZomboid = {
+			-- base informations
+			name = "IGUI_ZF_StrongZomboid",
+			chance = SandboxVars.ZF_Template.StrongZomboid_SpawnWeight,
+
+			-- stats
+			walktype = 1,
+			strength = 1,
+			toughness = 1,
+			cognition = 1,
+			memory = 1,
+			sight = 1,
+			hearing = 1,
+			HP = 20,
+
+			-- UI
+			color = {255, 0, 255,},
+			outline = {0, 0, 0,},
+
+			-- attack functions
+			zombieAgro = {},
+			zombieOnHit = {},
+
+			-- custom behavior
+			zombieDeath = {},
+			customBehavior = {},
+
+			customData = {
+				"SetRunnerSounds",
+			},
+		}
+	end
+end
+
+Events.OnGameStart.Add(Initialize_ZF_Template)
+
